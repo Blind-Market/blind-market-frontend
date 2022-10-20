a3eff0964f786722975ebabcbe010a8a2e21527f
kimmingyu <soil0201@gmail.com>
2022-10-20 12:35:26 +0900

    Blind Market Frontend
    Dark/Light 모드 버튼 토글 버튼으로 변경

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -55,40 +55,45 @@ export const Navbar = () => {
 						</span>
 					</a>
 				</Link>
+				<label
+					htmlFor="large-toggle"
+					className="inline-flex relative items-center cursor-pointer"
+				>
+					<input
+						type="checkbox"
+						value=""
+						id="large-toggle"
+						className="sr-only peer"
+						onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+					/>
+					<div className="w-14 h-7 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+					{theme === "dark" ? (
+						<svg
+							className="w-6 h-6 ml-2"
+							fill="white"
+							viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+						</svg>
+					) : (
+						<svg
+							className="w-6 h-6 ml-2"
+							fill="white"
+							viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								fillRule="evenodd"
+								d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+								clipRule="evenodd"
+							></path>
+						</svg>
+					)}
+					<span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
+				</label>
 				<div className={`w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
 					<div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start gap-2 flex flex-col lg:h-auto">
-						{windowSize > 768 ? (
-							<div
-								className="relative mr-3 md:mr-7 dark:bg-black bg-black"
-								onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-							>
-								{theme === "light" ? (
-									<svg
-										className="w-6 h-6"
-										fill="white"
-										viewBox="0 0 20 20"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-									</svg>
-								) : (
-									<svg
-										className="w-6 h-6"
-										fill="white"
-										viewBox="0 0 20 20"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											fillRule="evenodd"
-											d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-											clipRule="evenodd"
-										></path>
-									</svg>
-								)}
-							</div>
-						) : (
-							<></>
-						)}
 						<Link href="/write">
 							<button
 								className={`${
