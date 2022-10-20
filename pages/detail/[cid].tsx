2c93824963b029d269f8b652ff902feca050c49d
kimmingyu <soil0201@gmail.com>
2022-10-20 13:01:06 +0900

    Blind Market Frontend
    write page dark/light모드 적용

--- pages/detail/[cid].tsx
+++ pages/detail/[cid].tsx
@@ -1,5 +1,12 @@
 function Detail() {
-	return <></>;
+	return (
+		<div className="bg-blue-800 w-full h-full">
+			<div className="bg-red-200 w-full h-1/4"></div>
+			<div className="bg-red-300 w-full h-1/4"></div>
+			<div className="bg-red-400 w-full h-1/4"></div>
+			<div className="bg-red-500 w-full h-1/4"></div>
+		</div>
+	);
 }
 
 export default Detail;
